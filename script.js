7ca9ab0388d9e23774af9d9d1e4c13ce96e669d2
MICHAEL KINGS <57077025+MycoMK@users.noreply.github.com>
2021-01-25 01:07:43 -0800

    for-loopss

---
+++ script.js
@@ -0,0 +1,11 @@
+// alert('connected')
+
+// for-loops
+
+const names = ['pink','red','blue','yellow','orange']
+const colors = []
+for(let i = 0; i < names.length; i++){
+    colors.push(names[i].toUpperCase())
+}
+
+console.log(colors)
